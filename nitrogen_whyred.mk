0b76e3e41463f56df0b8051cfb4116b8f835430a
rahul1892 <kumar.rahul1892@gmail.com>
2019-06-03 13:03:16 +0530

    whyred: NOS bringup

--- nitrogen_whyred.mk
+++ nitrogen_whyred.mk
@@ -1,5 +1,5 @@
-# 
-# Copyright (C) 2018 The Mokee Project
+#
+# Copyright (C) 2019 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,20 +16,25 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
 # Inherit from whyred device
 $(call inherit-product, device/xiaomi/whyred/device.mk)
 
-# Inherit some common Mokee stuff.
-$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+# Inherit some NOS stuff
+$(call inherit-product, vendor/nitrogen/products/common.mk)
 
-PRODUCT_NAME := mk_whyred
+# Miui Camera
+$(call inherit-product-if-exists, vendor/MiuiCamera/config.mk)
+
+# Device identifier
+PRODUCT_NAME := nitrogen_whyred
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := whyred
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi Note 5
+TARGET_BOOT_ANIMATION_RES := 1080
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
